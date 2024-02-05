3ca55afe036ae4ecf112c64208da99011f372c9c
gracebir <grace.devolop@gmail.com>
2024-02-05 19:17:09 +0200

    header responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,13 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoSearchOutline } from "react-icons/io5";
+import { VscBell } from "react-icons/vsc";
 
 const Header = () => {
     const [isInputFocused, setIsInputFocused] = useState(false);
     return (
         <header className='bg-white sticky top-0 w-full h-[72px] z-50'>
-            <div className='max-w-[960px] mx-auto flex justify-between items-center h-full'>
+            <div className='max-w-[960px] mx-auto flex lg:justify-between justify-around items-center h-full'>
                 <div className='flex gap-4'>
                     <Link href={"#"}>
                         <Image
@@ -20,7 +21,7 @@ const Header = () => {
                             height={25.093}
                         />
                     </Link>
-                    <div className='flex items-center gap-1'>
+                    <div className='hidden lg:flex items-center gap-1'>
                         <RxHamburgerMenu
                             className='text-green-color'
                             size={16}
@@ -30,11 +31,11 @@ const Header = () => {
                 </div>
 
                 <div
-                    className={`border ${
+                    className={`lg:flex hidden border ${
                         isInputFocused
                             ? "border-green-color"
                             : "border-gray-300"
-                    } rounded-sm flex items-center px-3 py-2`}
+                    } rounded-sm items-center px-3 py-2`}
                 >
                     <button>
                         <IoSearchOutline size={20} />
@@ -48,7 +49,36 @@ const Header = () => {
                     />
                 </div>
 
-                <div></div>
+                <div className='lg:flex gap-2 hidden'>
+                    <Link href={"#"}>
+                        <Image
+                            src={"/assets/home-event.svg"}
+                            width={28}
+                            height={28}
+                            alt='indicator'
+                        />
+                    </Link>
+                    <Image
+                        src={"/assets/vertical-bar.svg"}
+                        width={1}
+                        height={14}
+                        alt='vertical-divider'
+                    />
+                    <Link className='text-sm text-text-color' href={"#"}>
+                        Login / Sign up
+                    </Link>
+                </div>
+
+                <div className='flex lg:hidden items-center text-gray-500'>
+                    <VscBell className='p-3 cursor-pointer' size={48} />
+                    <Image
+                        className='p-3 cursor-pointer'
+                        src={"/assets/search_new.svg"}
+                        width={42}
+                        height={44}
+                        alt='vertical-divider'
+                    />
+                </div>
             </div>
         </header>
     );
