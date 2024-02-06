e9242f9930b136d469163c92c3a5a060f92dcab8
gracebir <grace.devolop@gmail.com>
2024-02-06 08:52:03 +0200

    adding different section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import Categories from "@/components/Categories";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Shopping from "@/components/Shopping";
-import { section_1s } from "@/utils/data";
+import {
+    section_1s,
+    section_2s,
+    section_4s,
+    section_kitchen,
+    section_lg,
+    section_logitech,
+    section_macbook,
+} from "@/utils/data";
 
 export default function Home() {
     return (
@@ -18,7 +26,48 @@ export default function Home() {
                     title='HOT DEAL'
                     subTitle='[UP TO 34% OFF] HAPPY HOUR'
                 />
-                <Shopping title='New In' subTitle='#Notable new products' />
+                <Shopping
+                    title='New In'
+                    subTitle='#Notable new products'
+                    data={section_2s}
+                />
+                <Shopping
+                    data={section_1s}
+                    title='Benheim new store EVENT'
+                    subTitle='Lowest price guaranteed, 5% download coupon
+'
+                />
+                <Shopping
+                    title='Logitech after-sales service...'
+                    subTitle='#Guaranteed after-sales service as a genuine product, not a parallel import! return possible...'
+                    data={section_logitech}
+                />
+
+                <Shopping
+                    title='Thinner and more powerful LG Gram'
+                    subTitle='Additional discount coupon + 30-day trial fee reduction event'
+                    data={section_lg}
+                />
+                <Shopping
+                    title='Lowest price on gaming devices...'
+                    subTitle='#Limited quantity special price #Anticipated new work'
+                    data={section_2s}
+                />
+                <Shopping
+                    title='Electric Mat LAST BIG SALE'
+                    subTitle='UP TO 64% SALE'
+                    data={section_4s}
+                />
+                <Shopping
+                    title='Recommended Lunar New Year...'
+                    subTitle='UP TO 74% ▼'
+                    data={section_kitchen}
+                />
+                <Shopping
+                    title='MacBook clearance sale!'
+                    subTitle='Only in Test Valley! Try it for 30 days and buy it'
+                    data={section_macbook}
+                />
             </main>
         </div>
     );
