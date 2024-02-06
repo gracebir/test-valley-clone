e1effcd2d2d903570afcdccd3f3fa54de80fbc75
gracebir <grace.devolop@gmail.com>
2024-02-06 07:11:25 +0200

    product section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import Categories from "@/components/Categories";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Shopping from "@/components/Shopping";
+import { section_1s } from "@/utils/data";
 
 export default function Home() {
     return (
@@ -11,6 +13,12 @@ export default function Home() {
             <main className='max-w-[960px] mx-auto'>
                 <Hero />
                 <Categories />
+                <Shopping
+                    data={section_1s}
+                    title='HOT DEAL'
+                    subTitle='[UP TO 34% OFF] HAPPY HOUR'
+                />
+                <Shopping title='New In' subTitle='#Notable new products' />
             </main>
         </div>
     );
